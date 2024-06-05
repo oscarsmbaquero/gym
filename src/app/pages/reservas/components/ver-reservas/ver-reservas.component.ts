e0d2ecff89925481f7d58c68b5380835cf9a6bb9
oscar <oscarsmbaquero@gmail.com>
2024-06-05 19:00:29 +0200

    select categoria ok

--- src/app/pages/reservas/components/ver-reservas/ver-reservas.component.ts
+++ src/app/pages/reservas/components/ver-reservas/ver-reservas.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-ver-reservas',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatCardModule],
   templateUrl: './ver-reservas.component.html',
   styleUrl: './ver-reservas.component.scss'
 })
