227a4e8f6500db1274243d87c83eb5d57b3c48bf
oscar <oscarsmbaquero@gmail.com>
2024-06-12 10:39:34 +0200

    ajuste array horas

--- src/app/pages/reservas/components/anadir-reserva/anadir-reserva.component.ts
+++ src/app/pages/reservas/components/anadir-reserva/anadir-reserva.component.ts
@@ -59,7 +59,7 @@ export class AnadirReservaComponent implements OnInit {
   userName: string = '';
   userMail: string = '';
 
-  reservasDefecto =['09:30-11:00', '11:00-12:30', '12:30-14:00', '14:00-15:30', '15:30-17:00', '17:00-18:30', '18:30-20:00', '20:00-21:30', '21:30-23:00']
+  reservasDefecto =['09:30-11:00', '11:00-12:30', '12:30-14:00', '14:00-15:30', '15:30-17:00', '17:00-18:30', '18:30-20:00', '20:00-21:30']
 
   constructor(
     private formBuilder: FormBuilder,
